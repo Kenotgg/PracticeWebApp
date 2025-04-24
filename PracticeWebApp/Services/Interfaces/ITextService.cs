084cdb951fac1cee7d11603b91a9058163111906
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2025-04-24 17:37:46 +0500

    Bug fixes and correct response added.

---
+++ PracticeWebApp/Services/Interfaces/ITextService.cs
@@ -0,0 +1,8 @@
+﻿namespace PracticeWebApp.Services.Interfaces
+{
+    public interface ITextService
+    {
+         Task<string> ReturnProcessedString(string word);
+         public (bool, string) IsWordCorrect(string word);
+    }
+}
