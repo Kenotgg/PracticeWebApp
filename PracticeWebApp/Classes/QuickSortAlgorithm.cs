3fc920326fd6ab5f3df4e32e79151eea1cd25d57
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2025-05-05 22:28:02 +0500

    Задание 5

---
+++ PracticeWebApp/Classes/QuickSortAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿
+using PracticeWebApp.Services.Interfaces;
+
+namespace PracticeWebApp.Classes
+{
+    public class QuickSortAlgorithm : ISortAlgorithm
+    {
+        public string Sort(string input)
+        {
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return input; // Обработка пустой или null строки
+            }
+            char[] charArray = input.ToCharArray();
+            QuickSortAlphabeticalByCharArray(charArray, 0, charArray.Length - 1);
+            return new string(charArray);
+
+            
+        }
+        private static void QuickSortAlphabeticalByCharArray(char[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+                QuickSortAlphabeticalByCharArray(arr, left, pivotIndex - 1);
+                QuickSortAlphabeticalByCharArray(arr, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(char[] arr, int left, int right)
+        {
+            char pivot = arr[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (GetAlphabeticalIndex(arr[j]) <= GetAlphabeticalIndex(pivot))
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(char[] arr, int a, int b)
+        {
+            char temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        private static int GetAlphabeticalIndex(char c)
+        {
+            c = char.ToUpper(c);
+
+            if (!char.IsLetter(c))
+            {
+                return -1; // Обработка не-буквенных символов (можно изменить по желанию)
+            }
+
+            return c - 'A' + 1;
+        }
+
+       
+    }
+}
---
+++ PracticeWebApp/Classes/TreeSortAlgorithm.cs
@@ -0,0 +1,95 @@
+﻿using PracticeWebApp.Services.Interfaces;
+
+namespace PracticeWebApp.Classes
+{
+    public class TreeSortAlgorithm : ISortAlgorithm
+    {
+        private class Node
+        {
+            public char Data { get; set; }
+            public int Count { get; set; }  
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(char data)
+            {
+                Data = data;
+                Count = 1; 
+                Left = null;
+                Right = null;
+            }
+        }
+
+        private Node root;
+
+        public string Sort(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            root = null; 
+
+            
+            foreach (char c in input)
+            {
+                Insert(c);
+            }
+
+            
+            List<char> sortedChars = new List<char>();
+            InOrderTraversal(root, sortedChars);
+
+            return new string(sortedChars.ToArray());
+        }
+
+        private void Insert(char data)
+        {
+            root = InsertRecursive(root, data);
+        }
+
+        private Node InsertRecursive(Node root, char data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+
+            if (GetSortValue(data) < GetSortValue(root.Data))
+            {
+                root.Left = InsertRecursive(root.Left, data);
+            }
+            else if (GetSortValue(data) > GetSortValue(root.Data))
+            {
+                root.Right = InsertRecursive(root.Right, data);
+            }
+            else
+            {
+                
+                root.Count++;
+            }
+
+            return root;
+        }
+
+        private void InOrderTraversal(Node root, List<char> sortedChars)
+        {
+            if (root != null)
+            {
+                InOrderTraversal(root.Left, sortedChars);
+                for (int i = 0; i < root.Count; i++)  
+                {
+                    sortedChars.Add(root.Data);
+                }
+                InOrderTraversal(root.Right, sortedChars);
+            }
+        }
+
+        private int GetSortValue(char c)
+        {
+            return char.ToUpper(c) - 'A'; 
+        }
+
+    }
+}
